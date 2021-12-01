1bbb809b283e961ced6f48956b45216f32ed08ab
Taimoor khan <khant8k83@gmail.com>
2021-12-01 09:13:02 +0500

    frontend added

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -4,79 +4,109 @@ import { Form, Input, Grid, Card, Statistic } from 'semantic-ui-react';
 import { useSubstrate } from './substrate-lib';
 import { TxButton } from './substrate-lib/components';
 
-function Main (props) {
+import { black2AsHex } from "@polkadot/util-crypto"
+
+export function Main(props) {
+
   const { api } = useSubstrate();
   const { accountPair } = props;
 
-  // The transaction submission status
   const [status, setStatus] = useState('');
+  const [digest, setDigest] = useState('');
+  const [owner, setOwner] = useState('');
+  const [block, setBlock] = useState(0);
 
-  // The currently stored value
-  const [currentValue, setCurrentValue] = useState(0);
-  const [formValue, setFormValue] = useState(0);
+  let fileReader;
+
+  const bufferToDigest = () => {
+    const content = Array.from(new Uint8Array(fileReader.result)).map(b => b.toString(16).padStart(2, '0')).join();
+    const hash = black2AsHex(content, 256);
+    setDigest(hash);
+  }
+
+  // callback for when file is selected
+  const handleFileChoosen = () => {
+    const fileReader = new FileReader();
+    fileReader.onloadend = bufferToDigest;
+    fileReader.readAsArrayBuffer(file);
+  }
 
   useEffect(() => {
     let unsubscribe;
-    api.query.templateModule.something(newValue => {
-      // The storage value is an Option<u32>
-      // So we have to check whether it is None first
-      // There is also unwrapOr
-      if (newValue.isNone) {
-        setCurrentValue('<None>');
-      } else {
-        setCurrentValue(newValue.unwrap().toNumber());
-      }
+    api.query.TemplateModule.proofs(digest, result => {
+      setOwner(result[0].toString());
+      setBlock(result[1].toNumber());
     }).then(unsub => {
       unsubscribe = unsub;
     })
-      .catch(console.error);
+    return unsubscribe && unsubscribe
+  }, [Digest, api.query.templateModule])
 
-    return () => unsubscribe && unsubscribe();
-  }, [api.query.templateModule]);
-
+  function isClaimed() {
+    return block !== 0;
+  }
+  // The actual UI elements which are returned from our component.
   return (
-    <Grid.Column width={8}>
-      <h1>Template Module</h1>
-      <Card centered>
-        <Card.Content textAlign='center'>
-          <Statistic
-            label='Current Value'
-            value={currentValue}
-          />
-        </Card.Content>
-      </Card>
-      <Form>
+    <Grid.Column>
+      <h1>Proof of Existence</h1>
+      {/* Show warning or success message if the file is or is not claimed. */}
+      <Form success={!!digest && !isClaimed()} warning={isClaimed()}>
         <Form.Field>
+          {/* File selector with a callback to `handleFileChosen`. */}
           <Input
-            label='New Value'
-            state='newValue'
-            type='number'
-            onChange={(_, { value }) => setFormValue(value)}
+            type="file"
+            id="file"
+            label="Your File"
+            onChange={e => handleFileChosen(e.target.files[0])}
+          />
+          {/* Show this message if the file is available to be claimed */}
+          <Message success header="File Digest Unclaimed" content={digest} />
+          {/* Show this message if the file is already claimed. */}
+          <Message
+            warning
+            header="File Digest Claimed"
+            list={[digest, `Owner: ${owner}`, `Block: ${block}`]}
           />
         </Form.Field>
-        <Form.Field style={{ textAlign: 'center' }}>
+        {/* Buttons for interacting with the component. */}
+        <Form.Field>
+          {/* Button to create a claim. Only active if a file is selected, and not already claimed. Updates the `status`. */}
           <TxButton
             accountPair={accountPair}
-            label='Store Something'
-            type='SIGNED-TX'
+            label={'Create Claim'}
             setStatus={setStatus}
+            type="SIGNED-TX"
+            disabled={isClaimed() || !digest}
             attrs={{
               palletRpc: 'templateModule',
-              callable: 'doSomething',
-              inputParams: [formValue],
+              callable: 'createClaim',
+              inputParams: [digest],
+              paramFields: [true]
+            }}
+          />
+          {/* Button to revoke a claim. Only active if a file is selected, and is already claimed. Updates the `status`. */}
+          <TxButton
+            accountPair={accountPair}
+            label="Revoke Claim"
+            setStatus={setStatus}
+            type="SIGNED-TX"
+            disabled={!isClaimed() || owner !== accountPair.address}
+            attrs={{
+              palletRpc: 'templateModule',
+              callable: 'revokeClaim',
+              inputParams: [digest],
               paramFields: [true]
             }}
           />
         </Form.Field>
+        {/* Status message about the transaction. */}
         <div style={{ overflowWrap: 'break-word' }}>{status}</div>
       </Form>
     </Grid.Column>
   );
 }
 
-export default function TemplateModule (props) {
-  const { api } = useSubstrate();
-  return api.query.templateModule && api.query.templateModule.something
-    ? <Main {...props} />
-    : null;
+export default function TemplateModule(props) {
+  const { api } = useState();
+  return api.query.templateModule.proofs && api.query.templateModule.proofs ? (<Main />) : null;
 }
